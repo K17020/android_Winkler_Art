4b535f8f2c9b03af728208540823bca855c15e03
Kevin Tam <tam.kevin@hotmail.com>
2017-08-17 16:23:35 -0700

    First commit, setup basic layout for profile

---
+++ app/src/main/java/com/example/kevin/winklerart/winklerProfile.java
@@ -0,0 +1,13 @@
+package com.example.kevin.winklerart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class winklerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winkler_profile);
+    }
+}
